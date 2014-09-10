6fc1188d032aa4967b8ef910434cbcdae82144b4
jojofabe <jojofabe@vt.edu>
2014-09-10 14:18:38 -0400

    started to work on the parser part for the piet part, for this part of the program, we'll just be focusing on reading the image file and classifying the changes of hue, light and the type of color

--- lex.go
+++ lex.go
@@ -1,14 +1,71 @@
+//Temporary go file that parsers a piet image then classifies the pixels by using the
+//rules specified on this site: http://www.dangermouse.net/esoteric/piet.html
+
 //A lex parser in Go for the language Cool
-package lexer
+package main //figure out how to run a go file using another package name
 import (
-        "fmt"
-        "log"
-        "go/token"
-        "os"
+	"fmt"
+	//"go/token"
+	"image"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
+//lists all of the colors that will be represented in this language
+//props to gitpan/Piet-Interpreter, just following his
+//interpreter example to get started with this compiler
+var tableHexColors = map[string]string{
+	"FFC0C0": "light red", "FF0000": "red",
+	"FFFFC0": "light yellow", "FFFF00": "yellow",
+	"C0FFC0": "light green", "00FF00": "green",
+	"C0FFFF": "light cyan", "00FFFF": "cyan",
+	"C0C0FF": "light blue", "0000FF": "blue",
+	"FFC0FF": "light magenta", "FF00FF": "magenta",
+	"C00000": "dark red", "FFFFFF": "white",
+	"C0C000": "dark yellow", "000000": "black",
+	"00C000": "dark green",
+	"00C0C0": "dark cyan",
+	"0000C0": "dark blue",
+	"C000C0": "dark magenta",
+}
 
-//goes to the entire file and parser each of the tokens
-func parser(){
-  
+var tableHexCycle = map[string]int{
+	"FFC0C0": 0, "FF0000": 0,
+	"FFFFC0": 1, "FFFF00": 1,
+	"C0FFC0": 2, "00FF00": 2,
+	"C0FFFF": 3, "00FFFF": 3,
+	"C0C0FF": 4, "0000FF": 4,
+	"FFC0FF": 5, "FF00FF": 5,
+	"C00000": 0, "FFFFFF": -1,
+	"C0C000": 1, "000000": -1,
+	"00C000": 2,
+	"00C0C0": 3,
+	"0000C0": 4,
+	"C000C0": 5,
+}
+
+var tableHexLight = map[string]int{
+	"FFC0C0": 0, "FF0000": 1, "C00000": 2,
+	"FFFFC0": 0, "FFFF00": 1, "C0C000": 2,
+	"C0FFC0": 0, "00FF00": 1, "00C000": 2,
+	"C0FFFF": 0, "00FFFF": 1, "00C0C0": 2,
+	"C0C0FF": 0, "0000FF": 1, "0000C0": 2,
+	"FFC0FF": 0, "FF00FF": 1, "C000C0": 2,
+	"FFFFFF": -1, "000000": -1,
+}
+
+//goes to the entire file and parser each of the pixels and classifies the change of colors,
+//hues and light and outputs the changes to a new text
+//is void for now since we're just classifying the pixels
+func parser(fileName string) {
+	fmt.Println("Hello")
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please enter the file name") //to-do, print based on the error specified on the top of the file
+	} else {
+		parser(os.Args[1])
+	}
 }
