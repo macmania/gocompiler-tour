d671991178f165f38f6c2678d4dbe883750e42a4
jojofabe <jojofabe@vt.edu>
2014-09-17 19:27:48 -0400

    added first test on the lexer, very simple. Need to make it better

---
+++ lex_test.go
@@ -0,0 +1,35 @@
+//Need to be written in the other computer
+//tests the stack
+
+package lexer
+
+import (
+	"testing"
+)
+
+func TestPush(t *testing.T) {
+	stack := Stack("hello", nil)
+	push(stack, "hi")
+	if peek(stack) != "hi" {
+		t.Error("Expected 'hi', got ", peek(stack))
+	}
+}
+
+func TestPop(t *testing.T) {
+	stack := Stack("hello", nil)
+
+	if pop(stack) == true {
+		t.Error("Expected true", peek(stack))
+	}
+	if pop(stack) {
+		t.Error(peek(stack))
+	}
+	//if (isSuccess)
+	push(stack, "hi")
+	push(stack, "hoola")
+	pop(stack)
+	if peek(stack) != "hi" {
+		t.Error("Expected hi", peek(stack))
+	}
+	fmt.Println("Hello")
+}
